566d32bcd2591a2527f475d5fc418d5db808f245
Aakash Singh Rajput <94034414+aakash4dev@users.noreply.github.com>
2021-12-17 14:00:25 +0530

    making javascript shorter

---
+++ shot.js
@@ -0,0 +1,7 @@
+function out(value) {console.log(value)}
+function id(id){return document.getElementById(id);}
+function cl(cl){return document.getElementsByClassName(cl);}
+function tag(tagName) {return document.getElementsByTagName(tagName)}
+//.value
+function idValue(IDname) {return id(IDname).value}
+function clValue(c) {return cl(c).value}
