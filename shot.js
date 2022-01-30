47751933d88fa26077b9de5fa12b28554845b123
aakash4dev <aakash4dev.crypto@gmail.com>
2022-01-30 19:52:09 +0530

    copyright

--- shot.js
+++ shot.js
@@ -1,7 +1,24 @@
-function out(value) {console.log(value)}
-function id(id){return document.getElementById(id);}
-function cl(cl){return document.getElementsByClassName(cl);}
-function tag(tagName) {return document.getElementsByTagName(tagName)}
+/**
+ * shot.js - a javascript library make javascript short
+ * made by https://github.com/aakash4dev (MIT Licensed)
+ * https://github.com/aakash4dev/shot.js
+ */
+function out(value) {
+  console.log(value);
+}
+function id(id) {
+  return document.getElementById(id);
+}
+function cl(cl) {
+  return document.getElementsByClassName(cl);
+}
+function tag(tagName) {
+  return document.getElementsByTagName(tagName);
+}
 //.value
-function idValue(IDname) {return id(IDname).value}
-function clValue(c) {return cl(c).value}
+function idValue(IDname) {
+  return id(IDname).value;
+}
+function clValue(c) {
+  return cl(c).value;
+}
